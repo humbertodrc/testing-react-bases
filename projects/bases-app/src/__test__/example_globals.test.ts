0704506cfce5451a61ee155dcb895bf32d7c7a0b
danieldamian09 <humbertorivero9@gmail.com>
2023-01-23 20:42:48 -0300

    Test variables globals jest

---
+++ projects/bases-app/src/__test__/example_globals.test.ts
@@ -0,0 +1,13 @@
+describe('this is for group test cases', () => {
+  test('description', () => {
+    expect(true).toBe(true);
+  })
+  
+  test('description2', () => {
+    expect(true).toBe(true);
+  })
+  
+  test('description 3', () => {
+    expect(true).toBe(true);
+  })
+})
